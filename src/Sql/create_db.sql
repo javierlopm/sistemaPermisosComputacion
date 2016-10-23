2b23df93e91b40b561ca31024b809564d6b180a9
Javier <javierloplom@gmail.com>
2016-10-23 01:23:26 -0400

    newdb

--- src/Sql/create_db.sql
+++ src/Sql/create_db.sql
@@ -1,18 +1,42 @@
+/*
+    Datos de un estudiante
+ */
 CREATE TABLE IF NOT EXISTS estudiante (
     carnet     INTEGER PRIMARY KEY NOT NULL,
     nombre     TEXT                NOT NULL,
     telefono   TEXT                NOT NULL,
-    correo     TEXT                NOT NULL,
-    creditos_p INTEGER,
-    pp         INTEGER -- No hay soporte para bool
+    correo     TEXT                NOT NULL
 );
 
+/*
+    Tabla para superclase de permisos
+ */
 CREATE TABLE IF NOT EXISTS permiso (
+    id_permiso INTEGER PRIMARY KEY AUTO INCREMENT,
     fk_carnet  INTEGER NOT NULL,
     materia    TEXT    NOT NULL,
-    aprobado   INTEGER, -- No hay soporte para bool
+    tipo       CHAR(1) NOT NULL, -- No hay soporte para ENUMS
+    aprobado   CHAR(1),          -- No hay soporte para bool
+    trimestre  CHAR(1),          -- No hay soporte para ENUMS
+    anio       INTEGER,
     FOREIGN KEY (fk_carnet) REFERENCES estudiante(carnet),
-    PRIMARY KEY (fk_carnet,materia)
+);
+
+/*
+    Tabla para permisos para electivas
+ */
+CREATE TABLE IF NOT EXISTS permiso_materia (
+    id_permiso INTEGER NOT NULL,
+    materia    CHAR(8) NOT NULL,
+    FOREIGN KEY (id_permiso) REFERENCES permiso(id_permiso),
+    PRIMARY KEY (id_permiso)
+);
+
+CREATE TABLE IF NOT EXISTS permiso_creditos (
+    id_permiso   INTEGER NOT NULL,
+    num_creditos INTEGER NOT NULL,
+    FOREIGN KEY (id_permiso) REFERENCES permiso(id_permiso),
+    PRIMARY KEY (id_permiso)
 );
 
 -- INSERT INTO estudiante VALUES(
