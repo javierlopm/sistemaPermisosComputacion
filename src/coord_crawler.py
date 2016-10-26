0f771ee56f4bb6f4d8c700c9fed4d3248974a8ce
Javier <javierloplom@gmail.com>
2016-10-26 11:32:11 -0400

    added textbox to search window

--- src/coord_crawler.py
+++ src/coord_crawler.py
@@ -12,6 +12,17 @@ pattern.insert(0 , re.compile("\d{7}"))
 pattern.insert(1 , re.compile("\d{2}\-\d{5}"))
 pattern.insert(2 , re.compile("\d{2}\-\d{5}\@usb\.ve"))
 
+def format_id(student_id):
+    if   re.match(pattern[0],student_id):
+        return student_id
+    elif (re.match(pattern[1],student_id) or re.match(pattern[2],student_id)):
+        return student_id[0:2] + student_id[3:8]
+    else:
+        print ("Error: formato de carnet invalido para " + student_id + 
+               " probar con 0000000 , 00-00000 o 00-00000@usb.ve" )
+        # raise Exception("studen_id format error" ) 
+        return None
+
 class StudentDownloader():
     """Class for web crawler that search for students id and downloads
         their academic info
--- src/main_app.py
+++ src/main_app.py
@@ -2,6 +2,12 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+from coord_crawler import format_id,StudentDownloader
+from easygui       import msgbox
+import csv_creator
+import perm_store
+
+
 class SearchWindow(Gtk.Window):
 
     def __init__(self,old_window,name):
@@ -77,21 +83,25 @@ class InitWindow(Gtk.Window):
         grid.props.halign = Gtk.Align.CENTER
 
         grid.insert_column(0)
-
         grid.insert_row(0)
         grid.insert_row(1)
         grid.insert_row(2)
         grid.set_row_spacing(10)
         grid.set_column_spacing(30)
+
+        # Widgets
         label = Gtk.Label()
         label.set_text("Bienvenidos al sistema de permisos.\nSeleccione una opcion")
         label.set_justify(Gtk.Justification.CENTER)
+
+
 
         button1 = Gtk.Button(label="Descargar permisos")
         button2 = Gtk.Button(label="Iniciar programa de permisos")
 
         grid.attach(label,0,0,2,2)
         grid.attach(button1,0,5,2,2)
+        grid.attach(button2,0,25,2,2)
         grid.attach(button2,0,25,2,2)
 
         button1.connect("clicked", self.on_button1_clicked)
@@ -139,10 +149,14 @@ class LoginWindow(Gtk.Window):
 
 
 
-class MainWindow(Gtk.Window):
 
+class MainWindow(Gtk.Window):
+    """
+        Ventana principal de búsqueda de permisos y estudiantes
+    """
     def __init__(self):
         Gtk.Window.__init__(self, title="Hello World")
+        self.connect("delete-event", Gtk.main_quit)
 
         self.set_title("Permisos coordinación")
         self.set_default_size(320,200)
@@ -151,9 +165,13 @@ class MainWindow(Gtk.Window):
         self.grid = grid
         self.add(grid)
         grid.props.halign = Gtk.Align.CENTER
+        grid.set_row_spacing(10)
+        grid.set_column_spacing(50)
 
         grid.insert_row(0)
         grid.insert_row(1)
+        grid.insert_row(2)
+        grid.insert_row(3)
         grid.insert_column(0)
         grid.insert_column(1)
         grid.insert_column(2)
@@ -162,6 +180,20 @@ class MainWindow(Gtk.Window):
         button3 = Gtk.Button(label="Permisos de generales")
         button4 = Gtk.Button(label="Permisos de extra creditos")
         button5 = Gtk.Button(label="Permisos de PP")
+        self.student_entry = Gtk.Entry()
+        self.student_entry = Gtk.Entry()
+        # self.student_entry.set_text("Hello World")
+        # self.student_entry.set_text("Hello World")
+
+        inv_box = Gtk.Box(spacing=4)
+
+        inv_box.pack_start(self.student_entry,True,True,2)
+        inv_box.pack_start(self.student_entry,True,True,2)
+        inv_box.pack_start(button1,True,True,0)
+
+        # grid.attach(inv_box,1,5,1,1)
+
+
 
         label = Gtk.Label()
         label.set_text("Quedan permisos por procesar.")
@@ -169,22 +201,31 @@ class MainWindow(Gtk.Window):
 
 
 
+        # grid.attach(button1,0,20,2,2)
+        # grid.attach(student_entry,2,20,1,1)
+        grid.attach(inv_box,1,20,1,1)
+
         grid.attach(label  ,1,0,2,2)
-        grid.attach(button1,1,20,2,2)
-        grid.attach(button2,1,25,2,2)
-        grid.attach(button3,1,30,2,2)
-        grid.attach(button4,1,35,2,2)
-        grid.attach(button5,1,40,2,2)
+        grid.attach(button2,1,25,2,3)
+        grid.attach(button3,1,30,2,3)
+        grid.attach(button4,1,35,2,3)
+        grid.attach(button5,1,40,2,3)
 
 
-        button1.connect("clicked", self.on_button1_clicked)
+        button1.connect("clicked", self.on_student_clicked)
 
 
 
-    def on_button1_clicked(self, widget):
-        self.hide()
-        new_win = SearchWindow(self,"carnet")
-        response = new_win.show_all()
+    def on_student_clicked(self, widget):
+        formated_carnet = format_id(self.student_entry.get_text())
+
+        if formated_carnet:
+            self.hide()
+            new_win = StudentWindow(self,formated_carnet)
+            # new_win = SearchWindow(self,"carnet")
+            response = new_win.show_all()
+        else:
+            msgbox("Formato inválido, intente con 0000000,00-00000 ó 00-00000@usb.ve")
 
 
 win = InitWindow()
