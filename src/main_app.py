19fcab396e5bba83dc149c14fb3be052af071fd5
Javier <javierloplom@gmail.com>
2017-02-28 22:55:55 -0400

    agregado export

--- src/main_app.py
+++ src/main_app.py
@@ -4,7 +4,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk,GdkPixbuf,Gdk
 
 from coord_crawler import format_id,show_carnet,StudentDownloader,get_gen,get_elect
-from easygui       import msgbox,ccbox,filesavebox, choicebox
+from easygui       import msgbox,ccbox,filesavebox,choicebox,enterbox
 import os.path
 import sys
 from csv_creator import CsvCreator
@@ -333,6 +333,10 @@ class SearchWindow(HeaderBarWindow):
         self.fst_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL,spacing=0)
         main_box.pack_start(self.fst_box,True,True,0)
 
+        export_button = Gtk.Button(label="Exportar permisos")
+        export_button.connect("clicked", self.on_export_click)
+        main_box.pack_start(export_button,True,True,0)
+
         # scrollable view para permisos
         scrollable = Gtk.ScrolledWindow(vexpand=True)
         scrollable.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
@@ -362,6 +366,27 @@ class SearchWindow(HeaderBarWindow):
 
         self.update_missing_perms(self.count,self.count_a)
 
+    def on_export_click(self,widget):
+        from os import getenv
+        archivo = enterbox(msg='Lugar a exportar CSV'
+                          ,default=getenv('HOME')+'/export.csv'
+                          ,strip=False)
+        
+        export = "nombre,carnet,trimestre,anio,valor,tipo\n"
+        for p in self.std_perms:
+            # import pdb; pdb.set_trace()
+            export += ",".join(
+                [db.get_student(p['fk_carnet'])[0]["nombre"]
+                ,str(p['fk_carnet'])
+                ,Trimestre(p['trimestre']).memo_name()
+                ,str(p['anio'])
+                ,p['string_extra'] or str(p['int_extra']) or ""
+                ,TipoPermiso(str(p['tipo'])).name
+                ]) + "\n"
+
+        f = open(archivo, 'w')
+        f.write(export)
+        f.close()
 
     def update_missing_perms(self,new_val,new_val_a):
         if self.missing_count:
